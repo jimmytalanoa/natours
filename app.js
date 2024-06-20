66c507665a42f6a45c19b9c4d2559c04945ead1b
jimmytalanoa <jimmytalanoa@gmail.com>
2024-06-20 12:23:03 +1000

    Create base Pug template

--- app.js
+++ app.js
@@ -76,7 +76,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jimmy',
+  });
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
