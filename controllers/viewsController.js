8623d88749e981c274be19b2841518f7b33fa96b
jimmytalanoa <jimmytalanoa@gmail.com>
2024-06-24 00:04:20 +1000

    Added page to render errors, separating dev and prod errors

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // get tour data from collection
@@ -20,6 +21,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user',
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
   // build template
 
   // render template using data
