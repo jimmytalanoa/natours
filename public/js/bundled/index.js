787303768a6cc794122a8d15d52d2fd3ce0cd2e3
jimmytalanoa <jimmytalanoa@gmail.com>
2024-06-24 16:16:23 +1000

    added image upload to form

--- public/js/bundled/index.js
+++ public/js/bundled/index.js
@@ -169,12 +169,11 @@ if (loginForm) loginForm.addEventListener("submit", async (e)=>{
 if (logoutBtn) logoutBtn.addEventListener("click", (0, _login.logout));
 if (userDataForm) userDataForm.addEventListener("submit", (e)=>{
     e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    (0, _updateSettings.updateSettings)({
-        name,
-        email
-    }, "data");
+    const form = new FormData();
+    form.append("name", document.getElementById("name").value);
+    form.append("email", document.getElementById("email").value);
+    form.append("photo", document.getElementById("photo").files[0]);
+    (0, _updateSettings.updateSettings)(form, "data");
 });
 if (userPasswordForm) userPasswordForm.addEventListener("submit", async (e)=>{
     e.preventDefault();
