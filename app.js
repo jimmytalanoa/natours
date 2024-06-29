abe9b66fd308153a87600d922a3070bc1753ec7d
jimmytalanoa <jimmytalanoa@gmail.com>
2024-06-29 21:14:02 +1000

    Added heroku config

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+app.enable('trust proxy');
+
 // Assign PUG as the template engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
